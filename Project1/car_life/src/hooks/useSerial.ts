12895f59949899ce5fd7b6435c9820a0a3f739b0
jasonandjay <342690199@qq.com>
2020-07-28 17:30:24 +0800

    完善详情页面

--- Project1/car_life/src/hooks/useSerial.ts
+++ Project1/car_life/src/hooks/useSerial.ts
@@ -1,12 +1,44 @@
-import { reactive, toRefs } from '@vue/composition-api'
+import { reactive, toRefs, computed, UnwrapRef, watch } from '@vue/composition-api'
 import { getSerialDetail } from '../api/detail';
 
+interface IData {
+    serialDetail: {
+        [key: string]: any
+    },
+    years: string[],
+    curIndex: number,
+    // list: {}
+}
 export default () => {
-    const data = reactive({
-        serialDetail: {
+    const data: UnwrapRef<IData> = reactive({
+        serialDetail: {},
+        years: ["全部"],
+        curIndex: 0
+    })
+    const list = computed(() => {
+        const curYear = data.years[data.curIndex];
+        if (!data.serialDetail.list) {
+            return {};
+        }
+        let list = data.serialDetail.list;
+        if (curYear !== '全部') {
+            list = list.filter((item: any) => item.market_attribute.year === curYear);
+        }
+        list = sortSerialList(list);
+        return mergeSerialList(list)
+    })
 
+    watch([() => data.serialDetail], () => {
+        if (data.serialDetail.list) {
+            const years = data.serialDetail.list.map((item: any) => item.market_attribute.year);
+            years.sort((a: string, b: string) => Number(b) - Number(a));
+            data.years = ["全部"].concat(Array.from(new Set(years)));
         }
     })
+
+    function changeCurIndex(index: number) {
+        data.curIndex = index;
+    }
 
     async function getSerialDetailAction(SerialId: number) {
         const result = await getSerialDetail(SerialId)
@@ -15,8 +47,44 @@ export default () => {
         }
     }
 
+    function sortSerialList(list: any[]) {
+        return list.sort((a, b) => {
+            // 先按马力升序
+            if (a.exhaust === b.exhaust) {
+                // 如果相等，再按功率进行排序
+                if (a.max_power === b.max_power) {
+                    // 如果相等，再按吸气方式进行排序
+                    if (a.inhale_type > b.inhale_type) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                } else {
+                    return a.max_power - b.max_power
+                }
+            } else {
+                return a.exhuast - b.exhaust
+            }
+        })
+    }
+
+    function mergeSerialList(list: any[]) {
+        const obj: {[key: string]: any []} = {};
+        list.forEach((item) => {
+            const key = `${item.exhaust_str}/${item.max_power_str} ${item.inhale_type}`;
+            if (obj[key]) {
+                obj[key].push(item);
+            } else {
+                obj[key] = [item]
+            }
+        })
+        return obj;
+    }
+
     return {
         ...toRefs(data),
+        list,
+        changeCurIndex,
         getSerialDetailAction
     }
 }
--- Project1/car_life/src/views/Detail.vue
+++ Project1/car_life/src/views/Detail.vue
@@ -1,23 +1,211 @@
 <template>
+  <div v-if="Object.keys(serialDetail).length">
+    <section class="header">
+      <div class="img">
+        <img :src="serialDetail.CoverPhoto" alt />
+        <span>{{serialDetail.pic_group_count}}张照片</span>
+      </div>
+      <div class="price">
+        <p>
+          <span>{{serialDetail.market_attribute.dealer_price}}</span>
+          <span>{{serialDetail.market_attribute.official_refer_price}}</span>
+        </p>
+        <router-link tag="button" :to="`/dealer/${serialDetail.list[0].car_id}`">询问底价</router-link>
+      </div>
+    </section>
+    <section class="years" @click="changeYear">
+      <span v-for="(item, index) in years" :key="item" :class="{active: curIndex===index}">{{item}}</span>
+    </section>
+
+    <section>
+      <div class="list" v-for="(item, index) in list" :key="index">
+        <p>{{index}}</p>
+        <ul>
+          <router-link :to="`/dealer/${value.car_id}`" tag="li" v-for="(value, key) in item" :key="key">
+            <p>{{value.market_attribute.year+'款 '+value.car_name}}</p>
+            <p>{{value.horse_power+'马力'+value.gear_num+'档'+value.trans_type}}</p>
+            <p>
+              <span>{{'指导价'+value.market_attribute.official_refer_price}}</span>
+              <span>{{value.market_attribute.dealer_price_min+'起'}}</span>
+            </p>
+            <button>询问底价</button>
+          </router-link>
+        </ul>
+      </div>
+    </section>
+
+    <router-link tag="section" class="bottom" :to="`/dealer/${serialDetail.list[0].car_id}`">
+      <span>询问底价</span>
+      <span>本地经销商为你报价</span>  
+    </router-link>  
+  </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted} from '@vue/composition-api'
-import useSerial from '@/hooks/useSerial'
+import { defineComponent, onMounted } from "@vue/composition-api";
+import useSerial from "@/hooks/useSerial";
+
 
 export default defineComponent({
-  setup(props, {root: {$route: {params: {id}}}}) {
-
-      const {serialDetail, getSerialDetailAction} = useSerial();
+  setup(
+    props,
+    {
+      root: {
+        $route: {
+          params: { id },
+        },
+      },
+    }
+  ) {
+    const { serialDetail, getSerialDetailAction, years, curIndex, list, changeCurIndex } = useSerial();
     //   console.log('context...', id, serialDetail);
 
-      onMounted(() => {
-          getSerialDetailAction(Number(id))
-      })
+    onMounted(() => {
+      getSerialDetailAction(Number(id));
+    });
 
-      return {
-          serialDetail
+    function changeYear({target}: Event) {
+      if ((target as HTMLElement)?.nodeName === 'SPAN') {
+        const year = (target as HTMLElement)?.innerText;
+        changeCurIndex(years.value.findIndex((item) => item === year))
       }
-  }
-})
+    }
+
+    return {
+      serialDetail,
+      years,
+      curIndex,
+      list,
+      changeYear
+    };
+  },
+});
 </script>
+
+<style lang="scss" scoped>
+@import '../scss/_mixin.scss';
+
+    .img{
+        position: relative;
+        img{
+            width: 100%;
+        }
+        span{
+           position: absolute;
+            bottom: .3rem;
+            right: .3rem;
+            color: #fff;
+            padding: 1px .1rem;
+            border-radius: .2rem;
+            background: rgba(0,0,0,.6);
+            font-size: .24rem;
+        }
+    }
+    .price{
+        p{
+            span:nth-child(1){
+                font-size: .36rem;
+                color: red;
+                font-weight: 500;
+            }
+            span:nth-child(2){
+                font-size: .26rem;
+                color: silver;
+            }
+            @include flex(column, flex-start);
+        }
+        @include flex(row, space-between, center);
+        button{
+            border-radius: .1rem;
+            font-size: .32rem;
+            height: .7rem;
+            width: 3.45rem;
+            border: none;
+            box-sizing: border-box;
+            background: #00afff;
+            color: #fff;
+        }
+        padding: .36rem .2rem .3rem;
+    }
+    /deep/ .van-progress__pivot{
+        background-color: pink !important; 
+    }
+    .years span{
+      font-size: .32rem;
+      padding-right: .46rem;
+      line-height: .77rem;
+    }
+    .years .active{
+      color: #3aacff;
+    }
+    .list>p{
+      padding: 0 .2rem;
+      height: .5rem;
+      line-height: .5rem;
+      font-size: .26rem;
+      color: #999;
+      background: #f4f4f4;
+    }
+    .list li{
+        padding: 0 .2rem;
+        @include border1px();
+        overflow: hidden;
+        p:first-child{
+          padding: .26rem 0 .18rem;
+          font-size: .3rem;
+          line-height: 1;
+          color: #3d3d3d;
+        }
+        p:nth-child(2){
+          color: #bdbdbd;
+          font-size: .26rem;
+        }
+        p:nth-child(3){
+          text-align: right;
+            padding-bottom: .1rem;
+            font-size: .24rem;
+            color: #818181;
+            span:first-child{
+              text-align: right;
+              padding-bottom: .1rem;
+              font-size: .24rem;
+              color: #818181;
+            }
+            span:last-child{
+              font-size: .3rem;
+              color: #ff2336;
+              margin-left: .1rem;
+            }
+        }
+        button{
+          border: none;
+          border-top: 1px solid #eee;
+          @include border1px('top');
+          width: 110%;
+          height: .8rem;
+          font-size: .32rem;
+          color: #00afff;
+          background: #fff;
+          font-weight: 500;
+          margin-left: -.3rem;
+        }
+    }
+    .bottom{
+        position: fixed;
+        width: 100%;
+        bottom: 0;
+        z-index: 99;
+        background: #3aacff;
+        height: 1rem;
+        color: #fff;
+        @include flex(column, center, center);
+        span:first-child{
+          font-size: .32rem;
+          font-weight: 500; 
+        }
+        span:last-child{
+          margin-top: .12rem;
+          font-size: .24rem;
+        }
+    }
+</style>
